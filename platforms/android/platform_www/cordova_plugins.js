a3c65fe556dc018f1b3ccf901b3143d2d2fe44a9
amit.kremer <amit.kremer@appsflyer.com>
2021-01-20 18:26:21 +0200

    la

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,69 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    },
+    {
+      "id": "cordova-plugin-ionic-keyboard.keyboard",
+      "file": "plugins/cordova-plugin-ionic-keyboard/www/android/keyboard.js",
+      "pluginId": "cordova-plugin-ionic-keyboard",
+      "clobbers": [
+        "window.Keyboard"
+      ]
+    },
+    {
+      "id": "cordova-plugin-ionic-webview.IonicWebView",
+      "file": "plugins/cordova-plugin-ionic-webview/src/www/util.js",
+      "pluginId": "cordova-plugin-ionic-webview",
+      "clobbers": [
+        "Ionic.WebView"
+      ]
+    },
+    {
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
+    },
+    {
+      "id": "cordova-plugin-appsflyer-sdk.appsflyer",
+      "file": "plugins/cordova-plugin-appsflyer-sdk/www/appsflyer.js",
+      "pluginId": "cordova-plugin-appsflyer-sdk",
+      "clobbers": [
+        "window.plugins.appsFlyer"
+      ]
+    },
+    {
+      "id": "cordova-plugin-appsflyer-sdk.AppsFlyerError",
+      "file": "plugins/cordova-plugin-appsflyer-sdk/www/AppsFlyerError.js",
+      "pluginId": "cordova-plugin-appsflyer-sdk",
+      "clobbers": [
+        "AppsFlyerError"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-device": "2.0.2",
+    "cordova-plugin-ionic-keyboard": "2.2.0",
+    "cordova-plugin-ionic-webview": "4.2.1",
+    "cordova-plugin-splashscreen": "5.0.2",
+    "cordova-plugin-statusbar": "2.4.2",
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-appsflyer-sdk": "6.2.0"
+  };
+});
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Platform} from '@ionic/angular';
+
+declare var window;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    constructor(public platform: Platform) {
+        this.platform.ready().then(() => {
+            window.plugins.appsFlyer.registerOnAppOpenAttribution(function(res) {
+                    console.log('AppsFlyer OAOA ==> ' + res);
+                    alert('AppsFlyer OAOA ==> ' + res);
+                },
+                function onAppOpenAttributionError(err) {
+                    console.log(err);
+                });
+
+            let options = {
+                devKey: 'Us4Gm2SnJghcuoev846Qed',
+                isDebug: true,
+                appId: '741993991',
+                onInstallConversionDataListener: true,
+                waitForATTUserAuthorization: 10, //--> Here you set the time for the sdk to wait before launch
+            };
+
+            window.plugins.appsFlyer.initSdk(options, function(res) {
+                console.log('AppsFlyer GCD ==>' + res);
+                alert('AppsFlyer GCD ==> ' + res);
+
+            }, function(err) {
+                console.log(`AppsFlyer GCD ==> ${err}`);
+            });
+        });
+    }
 
 }
